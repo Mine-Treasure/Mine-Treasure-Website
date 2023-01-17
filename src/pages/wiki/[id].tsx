6d055139c49ce612112c465bf7d814b9cbffbf1d
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2023-01-17 21:49:17 +0100

    fix icons on wiki page

--- src/pages/wiki/[id].tsx
+++ src/pages/wiki/[id].tsx
@@ -77,7 +77,7 @@ const WikiSidebarSection = ({ sidebarSectionData, activePage }) => {
   return (
     <li>
       <button onClick={() => setIsDroppedDown(!isDroppedDown)} type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100">
-        {sidebarSectionData.icon && <FontAwesomeIcon icon={sidebarSectionData.icon} />}
+        {sidebarSectionData.icon && <FontAwesomeIcon height={20} icon={sidebarSectionData.icon} />}
         <span className="flex-1 ml-3 text-left whitespace-nowrap">{sidebarSectionData.title}</span>
         {sidebarSectionData.collapsable && <svg className={"w-6 h-6 transition duration-100 " + (!isDroppedDown ? "rotate-90" : "")} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>}
       </button>
